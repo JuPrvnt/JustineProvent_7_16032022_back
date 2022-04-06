3707792cf2c8c636e9362cefec956f4252ac610e
JuPrvnt <justine.provent90@gmail.com>
2022-04-06 16:42:10 +0200

    route afficher le profil

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -58,3 +58,11 @@ exports.logout = (req, res, next) => {
     .status(200)
     .send("L'utilisateur a été déconnecté");
 };
+
+// Récupération des données du profil
+exports.getOneUser = (req, res, next) => {
+  User.findOne({ _id: req.params.id })
+    .then((user) => res.status(200).json(user))
+    .catch((error) => res.status(404).json({ error }));
+  console.log(User);
+};
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -7,10 +7,14 @@ const router = express.Router();
 // Importation du controlleur user
 const userCtrl = require("../controllers/UserControllers");
 
+// Importation du middleware d'authentification
+const auth = require("../middleware/auth");
+
 // Route du endpoint signup
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
 router.get("/logout", userCtrl.logout);
+router.get("/:id", userCtrl.getOneUser);
 
 // Exportation du module router
 module.exports = router;
